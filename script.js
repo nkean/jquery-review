5cdbba6b86b99a684eb91037e5369b478a22f22d
nkean <nathankean2@gmail.com>
2018-04-12 14:16:29 -0500

    working delete button

--- script.js
+++ script.js
@@ -13,14 +13,21 @@ function onReady() {
     console.log( $('h2').text() );
 
     // Inputs (also, getter and setter)
-    $('#nameInput').val('Nathan'); // set value of #nameInput
-    console.log($('#nameInput').val()); // get value of #nameInput
+    // $('#nameInput').val('Nathan'); // set value of #nameInput
+    // console.log($('#nameInput').val()); // get value of #nameInput
 
     // Events
-    $('#clickableButton').on('click', function() { // event handler with anonymous function (no name)
-        console.log($('#nameInput').val());
+
+    $('#addPersonButton').on('click', function() { // event handler with anonymous function (no name)
+        let newPersonName = $('#nameInput').val();
+        $('#listOfNames').append('<li>' + newPersonName + ' ' + '<button class="deleteButton">Delete</button>'+ '</li>');
     });
-    
+
+    $('#listOfNames').on('click', '.deleteButton', function() {
+        console.log('Delete button was clicked');
+        $(this).parent().remove();
+    });
+
     $('#wasClicked').on('click', function() {
         console.log('I Was Clicked!');
     });
